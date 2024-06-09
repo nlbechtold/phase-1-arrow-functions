21e3c603192d27196de564cdbc047482c8cfe4d4
Nancy Bechtold <nancyleebechtold@gmail.com>
2024-06-09 17:09:22 -0600

    arrow lab

---
+++ index.js
@@ -0,0 +1,8 @@
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,3); //=> 5
+
+const square = (parameter1) => parameter1 * parameter1;
+square(4);
+
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+divide(2000,100);
